c557ad33023319e614eb8a3fbb3ad1354e541bca
HirayamaMasaki <masaki.hira1017@gmail.com>
2022-04-16 11:50:20 +0900

    再々々復旧(キーボード非表示の追加)

--- app/src/main/java/com/example/clouding_with_you/NewPoint.kt
+++ app/src/main/java/com/example/clouding_with_you/NewPoint.kt
@@ -1,10 +1,13 @@
 package com.example.clouding_with_you
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -74,6 +77,27 @@ class NewPoint : AppCompatActivity() {
         btnCurrentLocation.setOnClickListener {
             fetchLocation()
         }
+
+        etCityName.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+
+        etLat.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+
+        etLng.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
     }
 
     private fun fetchLocation() {
@@ -96,6 +120,12 @@ class NewPoint : AppCompatActivity() {
                 etLng.setText(it.longitude.toString(), TextView.BufferType.EDITABLE)
             }
         }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val focusView : TextView = findViewById(R.id.focusView)
+        focusView.requestFocus()
+        return super.dispatchTouchEvent(ev)
     }
 
 }
